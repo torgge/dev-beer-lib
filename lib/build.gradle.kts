29b90df02933f347baf5b13459ccb6de7ae02995
George Bonespirito <torgge@gmail.com>
2022-08-25 21:34:58 -0300

    update

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -23,8 +23,8 @@ publishing {
             name = "GitHubPackages"
             url = uri(project.findProperty("repo.url") as String)
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
